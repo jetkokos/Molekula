4137dbdad64ab1cff178638ad49b7b20d562579e
jetkokos <afeniks@bk.ru>
2018-09-01 16:15:24 +0300

    sending on e-mail

--- js/script.js
+++ js/script.js
@@ -1,15 +1,66 @@
 $(document).ready(function(){
+  //Плавное появление Popup
+    $(document).on("click", '.callBack_ref', function(event) {
+      event.preventDefault();
+      $('.callBack').addClass('visible');
+    });
+
+    $(document).on("click", '.takeCredit_ref', function(event) {
+      event.preventDefault();
+      $('.takeCredit').addClass('visible');
+    });
+
+    $(document).on("click", '.downloadCond_ref', function(event) {
+      event.preventDefault();
+      $('.downloadCond').addClass('visible');
+    });
+
+    $(document).on("click", '.downloadOffer_ref', function(event) {
+      event.preventDefault();
+      $('.downloadOffer').addClass('visible');
+    });
+
+    $(document).on("click", '.downloadContract_ref', function(event) {
+      event.preventDefault();
+      $('.downloadContract').addClass('visible');
+    });
+
+    $(document).on("click", '.downloadPolitics_ref', function(event) {
+      event.preventDefault();
+      $('.downloadPolitics').addClass('visible');
+    });
+
+    $(document).on("click", ".enterPop_ref", function(event) {
+      event.preventDefault();
+      $('.enterPop').addClass('visible');
+    });
+
+    $(document).on("click", "#close", function(event) {
+      $('.visible').removeClass('visible');
+    });
+
+
+/*Плавный переход к якорям*/
+    $(document).on("click", 'a[href^="#nav"]', function() {
+      var el = $(this).attr('href');
+      $('html, body').animate({
+      scrollTop: $(el).offset().top}, 1000);
+      console.log($(el));
+      return false;
+    });
+
+//Slick-Слайдер отзывов
   $('.slider-right').slick({
-  dots: true,
-  infinite: true,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: false,
-  appendDots: $('.slider-btns'),
-  variableWidth: true
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    appendDots: $('.slider-btns'),
+    variableWidth: true
   });
 
-  //E-mail Ajax Send
+//Отправка данных на почту
   $(".form").submit(function() { //Change
     var th = $(this);
     $.ajax({
@@ -19,117 +70,20 @@ $(document).ready(function(){
       processData: false,
       data: th.serialize()
     }).done(function() {
-      alert("Thank you!");
+        $('.visible').toggleClass('visible');
+        $('.popupSuccessCall').addClass('visible');
       setTimeout(function() {
         // Done Functions
         th.trigger("reset");
-      }, 1000);
+      }, 300);
     });
     return false;
   });  
-  
-/*Плавный переход к якорям*/
 
-  var linkNav = document.querySelectorAll('[href^="#nav"]'),
-    V = 0.5;  // скорость, может иметь дробное значение через точку
-    for (var i = 0; i < linkNav.length; i++) {
-  linkNav[i].addEventListener('click', function(e) {
-    e.preventDefault();
-    var w = window.pageYOffset,  // прокрутка
-        hash = this.href.replace(/[^#]*(.*)/, '$1');  // id элемента, к которому нужно перейти
-        t = document.querySelector(hash).getBoundingClientRect().top,  // отступ от окна браузера до id
-        start = null;
-    requestAnimationFrame(step); 
-    function step(time) {
-      if (start === null) start = time;
-      var progress = time - start,
-          r = (t < 0 ? Math.max(w - progress/V, w + t) : Math.min(w + progress/V, w + t));
-      window.scrollTo(0,r);
-      if (r != w + t) {
-        requestAnimationFrame(step)
-      } else {
-        location.hash = hash  // URL с хэшем
-      }
-    }
-  }, false);
-}
 
 
 });
 
-
-/*Плавное появление Popup*/
-
-function showCallBackPopup() {
-	document.querySelector(".callBack").classList.add('visible');
-};
-
-function hideCallBackPopup() {
-	document.querySelector(".callBack").classList.remove('visible');
-};
-
-function showTakeCreditPopup() {
-  document.querySelector(".takeCredit").classList.add('visible');
-};
-
-function hideTakeCreditPopup() {
-  document.querySelector(".takeCredit").classList.remove('visible');
-};
-
-function showDownloadCondPopup() {
-  document.querySelector(".downloadCond").classList.add('visible');
-};
-
-function hideDownloadCondPopup() {
-  document.querySelector(".downloadCond").classList.remove('visible');
-};
-
-function showDownloadOfferPopup() {
-  document.querySelector(".downloadOffer").classList.add('visible');
-};
-
-function hideDownloadOfferPopup() {
-  document.querySelector(".downloadOffer").classList.remove('visible');
-};
-
-function showDownloadContractPopup() {
-  document.querySelector(".downloadContract").classList.add('visible');
-};
-
-function hideDownloadContractPopup() {
-  document.querySelector(".downloadContract").classList.remove('visible');
-};
-
-function showDownloadPoliticsPopup() {
-  document.querySelector(".downloadPolitics").classList.add('visible');
-};
-
-function hideDownloadPoliticsPopup() {
-  document.querySelector(".downloadPolitics").classList.remove('visible');
-};
-function showEnterPopup() {
-  document.querySelector(".enterPop").classList.add('visible');
-};
-
-function hideEnterPopup() {
-  document.querySelector(".enterPop").classList.remove('visible');
-};
-
-function showPopupSuccessCall() {
-  document.querySelector(".popupSuccessCall").classList.add('visible');
-};
-
-function hidePopupSuccessCall() {
-  document.querySelector(".popupSuccessCall").classList.remove('visible');
-};
-
-function showPopupSuccessDocs() {
-  document.querySelector(".popupSuccessDocs").classList.add('visible');
-};
-
-function hidePopupSuccessDocs() {
-  document.querySelector(".popupSuccessDocs").classList.remove('visible');
-};
 
 
 
--- send.php
+++ send.php
@@ -1,8 +1,12 @@
 <?php 
-	use PHPMailer\PHPMailer\PHPMailer;
-
-	require 'PHPMailer.php';
-	require 'SMTP.php';
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
+    
+    require 'PHPMailer/PHPMailer.php';
+    require 'PHPMailer/SMTP.php';
+    require 'PHPMailer/Exception.php';
+    
 	// Переменные
 	$name = $_POST['name'];
 	$number = $_POST['tel'];
@@ -10,18 +14,19 @@
 	$mail = new PHPMailer;
 	try {
     //Server settings
-    $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+    $mail->SMTPDebug = 1;
+    $mail->CharSet = "utf-8";                                 // Enable verbose debug output
     $mail->isSMTP();                                      // Set mailer to use SMTP
     $mail->Host = 'smtp.yandex.ru';  					  // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'arkonaallods';                 // SMTP username
-    $mail->Password = '111111';                           // SMTP password
+    $mail->Username = 'arkonaallods';        // SMTP username
+    $mail->Password = 'Irjkf491';                           // SMTP password
     $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-    $mail->Port = 465;                                    // TCP port to connect to
+    $mail->Port = 587;                                    // TCP port to connect to
 
     //Recipients
-    $mail->setFrom('arkonaallod@yandex.ru');		  //My e-mail
-    $mail->addAddress('afenik@bk.ru');    			 // Add a recipient
+    $mail->setFrom('arkonaallods@yandex.ru');		  //My e-mail
+    $mail->addAddress('arkonaallods@yandex.ru');              // Add a recipient
     //$mail->addAddress('ellen@example.com');               // Name is optional
     //$mail->addReplyTo('info@example.com', 'Information');
     //$mail->addCC('cc@example.com');
@@ -34,8 +39,10 @@
     //Content
     $mail->isHTML(true);                                  // Set email format to HTML
     $mail->Subject = 'Заявка с Молекулы';
-    $mail->Body    = 'Имя $name . Телефон $number';
-    //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $body = '<p>Имя $name . Телефон $number</p>';
+    $mail->Body    = $body;
+    $mail->AltBody = strip_tags($body);
 
     $mail->send();
     echo 'Message has been sent';
